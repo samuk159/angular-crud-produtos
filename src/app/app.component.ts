70b773738f3a2bae0b8106935ef4fb83a684fada
samuk159 <samu_65@hotmail.com>
2021-02-11 09:03:59 -0200

    crud funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,50 @@ import { Produto } from './produto.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   produtos: Produto[] = [];
+  produto: Produto = new Produto(null, null);
+  indiceEdicao = -1;
 
   constructor() {
     this.produtos.push(new Produto("Camiseta", 30.99));
     this.produtos.push(new Produto("Calça Jeans", 100.99));
+  }
+
+  salvar() {
+    if (!this.produto.nome) {
+      console.log('Por favor informe o nome');
+      return;
+    }
+
+    if (!this.produto.preco) {
+      console.log('Por favor informe o preço');
+      return;
+    }
+
+    this.produto.preco = Number(
+      this.produto.preco.toString().replace(',', '.')
+    );
+
+    if (this.indiceEdicao > -1) {
+      this.produtos[this.indiceEdicao] = this.produto;
+    } else {
+      this.produtos.unshift(this.produto);
+    }
+    
+    this.produto = new Produto(null, null);
+    this.indiceEdicao = -1;
+  }
+
+  excluir(index) {
+    this.produtos.splice(index, 1);
+  }
+
+  editar(index) {
+    this.indiceEdicao = index;
+    this.produto = Object.assign(
+      new Produto(null, null), 
+      this.produtos[index]
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrecoPipe } from './preco.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PrecoPipe } from './preco.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
